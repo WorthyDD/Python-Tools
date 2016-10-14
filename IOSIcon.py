3294fc1b1c337d5e35eee047bdd0bb837703e224
duanwuxi <duanwuxi@xxkuaipao.com>
2016-10-14 17:56:53 +0800

    生成IOS图标, json转OC代码脚本

---
+++ IOSIcon.py
@@ -0,0 +1,16 @@
+from PIL import Image
+
+image = Image.open('test.jpg')
+
+image.thumbnail((1024,1024))
+image.save('1024.jpg')
+image.thumbnail((180,180))
+image.save('180.png')
+image.thumbnail((120,120))
+image.save('120.png')
+image.thumbnail((87,87))
+image.save('87.png')
+image.thumbnail((80,80))
+image.save('80.png')
+image.thumbnail((58,58))
+image.save('58.png')
